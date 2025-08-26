7f58ab6432c9c0cec2f1cca353c0176aa93b0802
Webster27400 <michal_stasinski@yahoo.com>
2025-08-26 21:49:17 +0200

    Implement full CRUD functionality for clients

--- clients/urls.py
+++ clients/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.client_list, name='client_list'),
+    path('create/', views.client_create, name='client_create'),
     path('<int:pk>/', views.client_detail, name='client_detail'),
+    path('<int:pk>/update/', views.client_update, name='client_update'),
+    path('<int:pk>/delete/', views.client_delete, name='client_delete'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,7 @@
 # clients/views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Client
+from .forms import ClientForm
 
 def client_list(request):
     clients = Client.objects.all()
@@ -15,3 +16,44 @@ def client_detail(request, pk):
         'client': client
     }
     return render(request, 'clients/client_detail.html', context)
+
+def client_create(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'clients/client_form.html', context)
+
+def client_update(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('client_detail', pk=client.pk)
+    else:
+        form = ClientForm(instance=client)
+    
+    context = {
+        'form': form,
+        'client': client
+    }
+    return render(request, 'clients/client_form.html', context)
+
+def client_delete(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        client.delete()
+        return redirect('client_list')
+    
+    context = {
+        'client': client
+    }
+    return render(request, 'clients/client_confirm_delete.html', context)
