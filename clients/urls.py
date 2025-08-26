bd860ffd823166dc7d2a70445b8be526e33b91f7
Webster27400 <michal_stasinski@yahoo.com>
2025-08-26 21:26:22 +0200

    Add client detail view template

--- clients/urls.py
+++ clients/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.client_list, name='client_list'),
+    path('<int:pk>/', views.client_detail, name='client_detail'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,5 @@
 # clients/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Client
 
 def client_list(request):
@@ -8,3 +8,10 @@ def client_list(request):
         'clients': clients
     }
     return render(request, 'clients/client_list.html', context)
+
+def client_detail(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    context = {
+        'client': client
+    }
+    return render(request, 'clients/client_detail.html', context)
